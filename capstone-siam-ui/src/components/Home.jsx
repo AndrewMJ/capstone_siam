828830f52272e1bcbccce9ff95390428eea8100c
acmarshall <ambercarriemarshall@gmail.com>
2018-06-13 18:26:06 -0400

    Fixed conflicts

--- capstone-siam-ui/src/components/Home.jsx
+++ capstone-siam-ui/src/components/Home.jsx
@@ -51,13 +51,7 @@ class Home extends Component {
 	showData()  {
 		return this.state.device.map((elem, index) => 
 
-<<<<<<< HEAD
-
-			<div className = "data col-sm-4" key={index}>
-			
-=======
 			<div className = "data col-sm-12" key={index}>
->>>>>>> 39ac46d5fb32fe1a6c608fce8c65848a130941cd
 				<div> 
 					<h6> Device Number: {elem.id} 
 					<div style = {{float: "right"}}>
@@ -67,11 +61,6 @@ class Home extends Component {
 						
 					</h6>
 					<h6> IP Address: {elem.ipAddr}
-<<<<<<< HEAD
-					</h6> 	
-
-					 <button onClick = { ()=> {this.message(elem.message)}} className = {this.buttonStatus(elem.message)}> Status </button>
-=======
 					<div style = {{float: "right"}}>
 						{elem.type === "printer" &&
 							<img className="icon" src='https://png.icons8.com/metro/1600/print.png' />
@@ -90,13 +79,11 @@ class Home extends Component {
 					</h6> 	
 					 	<button onClick = { ()=> { this.message(elem.message) } } className = {this.buttonStatus(elem.message)}> Status 
 					 	</button>
->>>>>>> 39ac46d5fb32fe1a6c608fce8c65848a130941cd
 					 <div style = {{float: "right"}}> 
 						<button onClick = { ()=> {this.showDetails(elem.id)}} className = "btn btn-sm btn-basic"> Details </button>	
 					 	</div>
 				</div>
 			</div>
-		
 			)
 	}
 
@@ -112,16 +99,15 @@ class Home extends Component {
 				swal(
 					'System is not connected',
 					"",
-					'warning'
+					'error'
 					)
 			}else {
 				swal('System is shutdown',
 					"",
-					'error')
+					'warning')
 			}	
 
 	}
-
 	buttonStatus(message){
 		const prefix = 'btn btn-sm btn-'
 		if(message == "SUCCESS"){
@@ -129,64 +115,21 @@ class Home extends Component {
 
 		}
 		else if(message == "IP_DEST_HOST_UNREACHABLE"){
-			return prefix + 'warning'
-			
-		}
-		else{
 			return prefix + 'danger'
 			
 		}
-	}
-
-<<<<<<< HEAD
-	count(message){
-		const prefix = "You have "
-		const ending = " devices "
-		let success = 0
-		let shutdown = 0
-		let broken = 0
-		
-		if(message == "SUCCESS"){
-			success++
-			return prefix + success + ending + "connected"
-
-		}
-		else if(message == "IP_DEST_HOST_UNREACHABLE"){
-			shutdown++
-			return prefix + shutdown + ending + "shutdown"
-			
-		}
 		else{
-			broken++
-			return prefix + broken + ending + "not working"
+			return prefix + 'warning'
 			
 		}
 	}
 
-	// deviceImage(id){
-	// 	axios.get(`http://35.237.107.174:8080/data/${id}`)
-	// 	.then((res) => {
-	// 		if (res.data.type == "printer"){
-	// 			console.log(true)
-	// 			 this.setState({deviceType:'print'})
-	// 		}
-
-	// 	})
-	// 	.catch((error)=>{
-	// 		console.log(error); 
-	// 	})
-	// 	return imagePath;
-	// }
-
-=======
->>>>>>> 39ac46d5fb32fe1a6c608fce8c65848a130941cd
 	showDetails(id){
 		axios.get(`http://35.237.107.174:8080/data/${id}`)
 		.then((res) => {
-			let details = `<h5> Device Details for IP: ${res.data.ipaddr}<br>
-			<br>
-			Last Connected: ${res.data.last_connected}<br>
+			let details = `<h5> ID: ${res.data.id}<br>
 			Mac Address: ${res.data.macaddr}<br> 
+			IP Address: ${res.data.ipaddr}<br>
 			Company: ${res.data.company}<br>
 			Type: ${res.data.type}<br>
 			Speed: ${res.data.rtt} ms per packet <br> 
@@ -208,13 +151,9 @@ class Home extends Component {
 			<div>
 				<div className = "row">
 					<div className = "col-sm-12 text-center">
-<<<<<<< HEAD
-						<h2> Devices - Location SIAM </h2>
-=======
 						<h2> Shared Spaces Devices </h2>
 						<h3> Currently Connected Devices: {this.getCount()}</h3>
 						
->>>>>>> 39ac46d5fb32fe1a6c608fce8c65848a130941cd
 					</div>
 				</div>
 				<div className = "container">
